4ac3dd11e8f74f85bdc5ac3b00aa91e9655c1cdf
hdugan <hilarydugan@gmail.com>
2023-03-04 14:39:59 -0600

    watershed area

--- src/ntl_lake.R
+++ src/ntl_lake.R
@@ -18,12 +18,12 @@ locations =
   data.frame(lakeid = 'BM', lat = 46.021067, long = -89.611783, avg_depth_m = 7.5, max_depth_m = 21.3, mixing_type = 'dimictic', waterbody_area_ha = 363.4, elevation_m = 500, watershed_area_ha = 555.6)) |> bind_rows(
   data.frame(lakeid = 'CB', lat = 46.007583, long = -89.606183, avg_depth_m = 1.7, max_depth_m = 2.5, mixing_type = 'dimictic', waterbody_area_ha = 0.6, elevation_m = 501, watershed_area_ha = NA)) |> bind_rows(
   data.frame(lakeid = 'CR', lat = 46.00275, long = -89.612233, avg_depth_m = 11.4, max_depth_m = 20.4, mixing_type = 'dimictic', waterbody_area_ha = 37.5, elevation_m = 500, watershed_area_ha = NA)) |> bind_rows(
-  data.frame(lakeid = 'SP', lat = 46.007733, long = -89.701183, avg_depth_m = 10.9, max_depth_m = 20, mixing_type = 'dimictic', waterbody_area_ha = 64, elevation_m = 491, watershed_area_ha = 237.4)) |> bind_rows(
+  data.frame(lakeid = 'SP', lat = 46.007733, long = -89.701183, avg_depth_m = 10.9, max_depth_m = 20, mixing_type = 'dimictic', waterbody_area_ha = 64, elevation_m = 495, watershed_area_ha = 237.4)) |> bind_rows(
   data.frame(lakeid = 'TB', lat = 46.04125, long = -89.686283, avg_depth_m = 5.6, max_depth_m = 7.9, mixing_type = 'dimictic', waterbody_area_ha = 	1.0, elevation_m = 495, watershed_area_ha = NA)) |> bind_rows(
   data.frame(lakeid = 'TR',	lat = 46.029267, long = -89.665017, avg_depth_m = 14.6, max_depth_m = 35.7, mixing_type = 'dimictic', waterbody_area_ha = 1565.1, elevation_m = 490, watershed_area_ha = NA)) |> bind_rows(
-  data.frame(lakeid = 'ME', lat = 43.09885, long = -89.40545, avg_depth_m = 12.8, max_depth_m = 25.3, mixing_type = 'dimictic', waterbody_area_ha = 3961.2, elevation_m = 259, watershed_area_ha = NA)) |> bind_rows(
-  data.frame(lakeid = 'MO', lat = 43.06337, long = -89.36086, avg_depth_m = 8.2, max_depth_m = 22.5, mixing_type = 'dimictic', waterbody_area_ha = 1359.8, elevation_m = 254, watershed_area_ha = NA)) |> bind_rows(
-  data.frame(lakeid = 'WI', lat = 43.05258, long = -89.42499, avg_depth_m = 2.7, max_depth_m = 4, mixing_type = 'polymictic', waterbody_area_ha = 136.2, elevation_m = 254, watershed_area_ha = NA)) |> bind_rows(
+  data.frame(lakeid = 'ME', lat = 43.09885, long = -89.40545, avg_depth_m = 12.8, max_depth_m = 25.3, mixing_type = 'dimictic', waterbody_area_ha = 3961.2, elevation_m = 259, watershed_area_ha = 29175)) |> bind_rows(
+  data.frame(lakeid = 'MO', lat = 43.06337, long = -89.36086, avg_depth_m = 8.2, max_depth_m = 22.5, mixing_type = 'dimictic', waterbody_area_ha = 1359.8, elevation_m = 254, watershed_area_ha = 24946)) |> bind_rows(
+  data.frame(lakeid = 'WI', lat = 43.05258, long = -89.42499, avg_depth_m = 2.7, max_depth_m = 4, mixing_type = 'polymictic', waterbody_area_ha = 136.2, elevation_m = 254, watershed_area_ha = 1398)) |> bind_rows(
   data.frame(lakeid = 'FI', lat = 43.28733, long = -89.65173, avg_depth_m = 6.6, max_depth_m = 18.9, mixing_type = 'dimictic', waterbody_area_ha = 80.4, elevation_m = 259, watershed_area_ha = NA))
 
   
